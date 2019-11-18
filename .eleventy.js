571df1e6535fcaadd531b795d567d723c00371dd
Steve Barnett <steve@naga.co.za>
2019-11-18 17:41:12 +0200

    Add Netlify CMS POC

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("downloads");
   eleventyConfig.addPassthroughCopy("fonts");
@@ -6,4 +6,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("video");
   eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("admin/config.yml");
+
+  eleventyConfig.addFilter("markdownify", function(content) {
+    var MarkdownIt = require("markdown-it");
+    var md = new MarkdownIt();
+    var result = md.render(content);
+    return result;
+  });
 };
