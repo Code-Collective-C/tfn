28578c5b4d01f247d7e3c2e9ba683b4ea0b5c434
Chante van der Walt <chantevdwalt87@gmail.com>
2020-09-10 09:21:20 +0200

    Website changes: -Change Covid Banner Color & Move to Top - Banner Image - Sticky Nav - CTA Color change - Center H1 page titles - Add Renati Tech in Footer - Add links to Footer Logos - Change Footer Color - Products & Services -- New brochures -- Changed Read More Pages -- Removed Old Information -- Added New Information -- Changed Images - Added & Changed Management Team -  Changed Color on FAQ's - Added padding to FAQ's - Added The Well To Community - Changed Bubble Color & Added Padding - Changed Contact Form & Button Color

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,59 @@
-$(function() {
-  var $el,
-    leftPos,
-    newWidth,
-    $mainNav = $(".navbar-nav");
+// $(function() {
+//   var $el,
+//     leftPos,
+//     newWidth,
+//     $mainNav = $(".navbar-nav");
+//
+//   $mainNav.append("<li id='magic-line'></li>");
+//   var $magicLine = $("#magic-line");
+//
+//   $magicLine
+//     .width($(".active").width())
+//     .css("left", $(".active a").position().left)
+//     .data("origLeft", $magicLine.position().left)
+//     .data("origWidth", $magicLine.width());
+//
+//   $(".navbar-nav li a").hover(
+//     function() {
+//       $el = $(this);
+//       leftPos = $el.position().left;
+//       newWidth = $el.parent().width();
+//       $magicLine.stop().animate({
+//         left: leftPos,
+//         width: newWidth
+//       });
+//     },
+//     function() {
+//       $magicLine.stop().animate({
+//         left: $magicLine.data("origLeft"),
+//         width: $magicLine.data("origWidth")
+//       });
+//     }
+//   );
+// })
 
-  $mainNav.append("<li id='magic-line'></li>");
-  var $magicLine = $("#magic-line");
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-  $magicLine
-    .width($(".active").width())
-    .css("left", $(".active a").position().left)
-    .data("origLeft", $magicLine.position().left)
-    .data("origWidth", $magicLine.width());
+// Get the navbar
+let navbar = document.getElementById("navbar");
+let navbarContainer = document.getElementById("navbar-container");
 
-  $(".navbar-nav li a").hover(
-    function() {
-      $el = $(this);
-      leftPos = $el.position().left;
-      newWidth = $el.parent().width();
-      $magicLine.stop().animate({
-        left: leftPos,
-        width: newWidth
-      });
-    },
-    function() {
-      $magicLine.stop().animate({
-        left: $magicLine.data("origLeft"),
-        width: $magicLine.data("origWidth")
-      });
+// Get the offset position of the navbar
+// let sticky = navbarContainer.offsetScrollActivate;
+let sticky = 475;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+        navbarContainer.classList.remove("scrolled-top");
+        navbar.classList.remove("navbar-not-scrolled")
+        navbar.classList.add("navbar-scrolled");
+    } else {
+        navbar.classList.remove("sticky");
+        navbarContainer.classList.add("scrolled-top");
+        navbar.classList.add("navbar-not-scrolled")
+        navbar.classList.remove("navbar-scrolled");
     }
-  );
-})
+}
